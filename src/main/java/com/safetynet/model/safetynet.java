1f47933caa5fb0c4f83a57e93cc07c89a8c8a1f1
Tarpantone <127950834+Tarpantone@users.noreply.github.com>
2023-09-10 18:10:17 +0200

    ajout de la description à l'objet model safetynet

--- src/main/java/com/safetynet/model/safetynet.java
+++ src/main/java/com/safetynet/model/safetynet.java
@@ -4,7 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-
+/*safetynet est le model de notre fichier json comprenant
+une liste de person, une liste de medicalrecord et une liste de firestation*/
 @Data
 @NoArgsConstructor
 public class safetynet {
