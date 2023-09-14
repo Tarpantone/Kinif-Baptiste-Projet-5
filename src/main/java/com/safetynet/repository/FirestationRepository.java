0cc75a62d0ac0ebd33f8c48eb745a1f7dcffbd1b
Tarpantone <127950834+Tarpantone@users.noreply.github.com>
2023-09-14 21:32:11 +0200

    Ajout des méthodes delete update et add

--- src/main/java/com/safetynet/repository/FirestationRepository.java
+++ src/main/java/com/safetynet/repository/FirestationRepository.java
@@ -3,7 +3,6 @@ package com.safetynet.repository;
 import com.safetynet.model.Firestation;
 import org.springframework.stereotype.Repository;
 
-import java.io.File;
 import java.util.List;
 
 @Repository
@@ -15,4 +14,20 @@ public class FirestationRepository {
         jsonDataGetter.dataGetter();
         return jsonDataGetter.getSafetynet().getFirestations();
     }
+
+    public List <Firestation> getAllFirestation(){
+        return this.firestations;
+    }
+
+    public boolean deleteFirestation(String address, int caserneID){
+        return firestations.remove(new Firestation(address,caserneID));
+    }
+
+    public boolean addFirestation(String address,int caserneID){
+        return firestations.add(new Firestation(address,caserneID));
+    }
+
+    /*public boolean updateFirestation(String address,int caserneID){
+        return ()
+    }*/
 }
--- src/main/java/com/safetynet/repository/MedicalrecordRepository.java
+++ src/main/java/com/safetynet/repository/MedicalrecordRepository.java
@@ -9,4 +9,28 @@ import java.util.List;
 public class MedicalrecordRepository {
     List <Medicalrecord> medicalrecords;
     JsonDataGetter jsonDataGetter;
+
+    public List<Medicalrecord> getMedicalrecordsFromSafetynet(){
+        jsonDataGetter.dataGetter();
+        return jsonDataGetter.getSafetynet().getMedicalrecords();
+    }
+
+    public boolean deleteMedicalrecords(String firstname,String lastname){
+        boolean result=false;
+        for (Medicalrecord mr:medicalrecords){
+            if (mr.getFirstName().equals(firstname)&&mr.getLastName().equals(lastname)){
+                result=medicalrecords.remove(mr);
+                break;
+            }
+        }
+        return result;
+    }
+
+    public boolean addMedicalrecords(String firstname, String lastname, String birthdate,List<String>medications,List<String>allergies){
+        return medicalrecords.add(new Medicalrecord(firstname,lastname,birthdate,medications,allergies));
+    }
+
+    public boolean updateMedicalrecords(String firstname, String lastname, String birthdate,List<String>medications,List<String>allergies){
+        return (deleteMedicalrecords(firstname,lastname)&&addMedicalrecords(firstname,lastname,birthdate,medications,allergies));
+    }
 }
